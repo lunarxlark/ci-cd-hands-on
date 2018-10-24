b7a13df5059bc259af025b8d2a6ea4047bceaa04
lunarxlark <lunarxlark@gmail.com>
2018-10-24 20:43:11 +0900

    fix bug

--- src/model/fizzbuzz.js
+++ src/model/fizzbuzz.js
@@ -3,7 +3,7 @@ module.exports = (number) => {
 
   for (let i = 1; i <= number; i++) {
     if (i % 15 == 0) {
-      result.push("FizzBuzzaaa");
+      result.push("FizzBuzz");
     } else if (i % 3 == 0) {
       result.push("Fizz");
     } else if (i % 5 == 0) {
